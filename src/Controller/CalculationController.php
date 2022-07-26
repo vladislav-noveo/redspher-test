a540b7d9b8cc15e6f694abab9bdb33212801fc39
Vladislav Zherdev <vladislav.zherdev@noveogroup.com>
2022-07-26 23:22:52 +0400

    Fixed README, removed odd imports

--- src/Controller/CalculationController.php
+++ src/Controller/CalculationController.php
@@ -5,16 +5,11 @@ namespace App\Controller;
 use App\DTO\CalculationDTO;
 use App\Form\Type\CalculationDTOType;
 use App\Service\CalculationService;
-use App\Validator\ValidMathProblem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 final class CalculationController extends AbstractController
 {
